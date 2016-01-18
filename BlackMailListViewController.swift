a490fdba8b4c585902985ed13863d761d6b8a287
Regina Imhoff <regina.imhoff@gmail.com>
2016-01-18 16:09:10 -0600

    start over w/ blackmail

---
+++ BlackMailListViewController.swift
@@ -0,0 +1,37 @@
+//
+//  BlackMailListViewController.swift
+//  BlackMail
+//
+//  Created by Regina Imhoff on 1/7/16.
+//  Copyright © 2016 Regina Imhoff. All rights reserved.
+//
+
+import UIKit
+
+class BlackMailListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let myFriends = ["Joanna", "Joyanna", "Jonathan", "Jinger", "Jennifer", "Josie", "Brooklyn"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        self.tableView.dataSource = self
+        self.tableView.delegate = self
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.myFriends.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        cell.textLabel!.text = myFriends[indexPath.row]
+        
+        return cell
+    }
+}
---
+++ FriendDetailViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FriendDetailViewController.swift
+//  BlackMail
+//
+//  Created by Regina Imhoff on 1/18/16.
+//  Copyright © 2016 Regina Imhoff. All rights reserved.
+//
+
+import UIKit
+
+class FriendDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
